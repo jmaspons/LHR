6e84fb7dfe4ddc0e5d5cb2eda6a028d942eeffdf
Joan Maspons <joanmaspons@gmail.com>
2012-07-17 13:11:01 +0200

    n,a,b -> size,shape1,shape2

--- src/betabinom.c
+++ src/betabinom.c
@@ -37,32 +37,32 @@
 #include "dpq.h"
 #include "R_ext/Visibility.h"
 
-double attribute_hidden dbetabinom_raw(double x, double n, double a, double b, int give_log)
+double attribute_hidden dbetabinom_raw(double x, double size, double shape1, double shape2, int give_log)
 {
     double ans;
 
-    if (x == 0 && n == 0) return R_D__1;
-    if (x < 0 || x > n) return R_D__0;
+    if (x == 0 && size == 0) return R_D__1;
+    if (x < 0 || x > size) return R_D__0;
 
-    ans = choose(n,x) * beta(x+a, n-x+b) / beta(a, b);
+    ans = choose(size,x) * beta(x+shape1, size-x+shape2) / beta(shape1, shape2);
 
     return R_D_qIv(ans);
 }
 
-double dbetabinom(double x, double n, double a, double b, int give_log)
+double dbetabinom(double x, double size, double shape1, double shape2, int give_log)
 {
 #ifdef IEEE_754
     /* NaNs propagated correctly */
-    if (ISNAN(x) || ISNAN(n) || ISNAN(a) || ISNAN(b)) return x + n + a + b;
+    if (ISNAN(x) || ISNAN(size) || ISNAN(shape1) || ISNAN(shape2)) return x + size + shape1 + shape2;
 #endif
 
-    if (a <= 0 || b <= 0 || R_D_negInonint(n)) return R_NaN;
+    if (shape1 <= 0 || shape2 <= 0 || R_D_negInonint(size)) return R_NaN;
     R_D_nonint_check(x);
     if (x < 0 || !R_FINITE(x)) return R_D__0;
 
-    n = R_D_forceint(n);
+    size = R_D_forceint(size);
     x = R_D_forceint(x);
-    return dbetabinom_raw(x, n, a, b, give_log);
+    return dbetabinom_raw(x, size, shape1, shape2, give_log);
 }
 
 /*
@@ -72,34 +72,34 @@ double dbetabinom(double x, double n, double a, double b, int give_log)
  *    The distribution function of the beta binomial distribution.
  */
 
-double attribute_hidden pbetabinom_raw(double x, double n, double a, double b, int log_p)
+double attribute_hidden pbetabinom_raw(double x, double size, double shape1, double shape2, int log_p)
 {
     int i;
     double ans = 0;
     for (i = 0; i <= x; ++i) {
-        ans += dbetabinom_raw(i, n, a, b, log_p);
+        ans += dbetabinom_raw(i, size, shape1, shape2, log_p);
     }
     return ans;
 }
 
-double pbetabinom(double x, double n, double a, double b, int lower_tail, int log_p)
+double pbetabinom(double x, double size, double shape1, double shape2, int lower_tail, int log_p)
 {
 #ifdef IEEE_754
-    if (ISNAN(x) || ISNAN(n) || ISNAN(a) || ISNAN(b))
-        return x + n + a + b;
-    if (!R_FINITE(n)) return R_NaN; //TODO || !R_FINITE(shape1) || !R_FINITE(shape2)
+    if (ISNAN(x) || ISNAN(size) || ISNAN(shape1) || ISNAN(shape2))
+        return x + size + shape1 + shape2;
+    if (!R_FINITE(size)) return R_NaN; //TODO || !R_FINITE(shape1) || !R_FINITE(shape2)
 #endif
 
-    if (a <= 0 || b <= 0 || R_D_nonint(n)) return R_NaN;
-    n = R_D_forceint(n);
+    if (shape1 <= 0 || shape2 <= 0 || R_D_nonint(size)) return R_NaN;
+    size = R_D_forceint(size);
 
-    if (n < 0) return R_NaN;
+    if (size < 0) return R_NaN;
 
     if (x < 0) return R_DT_0;
     x = floor(x + 1e-7);
-    if (n <= x) return R_DT_1;
+    if (size <= x) return R_DT_1;
 
-    double ans = pbetabinom_raw(x, n, a, b, log_p);
+    double ans = pbetabinom_raw(x, size, shape1, shape2, log_p);
     if (!lower_tail) ans = 1 - ans;
     return ans;
 }
@@ -117,7 +117,7 @@ double pbetabinom(double x, double n, double a, double b, int lower_tail, int lo
  */
 
 static double
-do_search(double y, double *z, double p, double n, double a, double b, double incr)
+do_search(double y, double *z, double p, double size, double shape1, double shape2, double incr)
 {
     if(*z >= p) {
         /* search to the left */
@@ -130,7 +130,7 @@ do_search(double y, double *z, double p, double n, double a, double b, double in
         for(;;) {
             double newz;
             if(y == 0 ||
-                    (newz = pbetabinom(y - incr, n, a, b, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
+                    (newz = pbetabinom(y - incr, size, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
                 return y;
             y = fmax2(0, y - incr);
             *z = newz;
@@ -147,9 +147,9 @@ do_search(double y, double *z, double p, double n, double a, double b, double in
         REprintf("\tnew z=%7g < p = %7g  --> search to right (y++) ..\n", z,p);
 #endif
         for(;;) {
-            y = fmin2(y + incr, n);
-            if(y == n ||
-                    (*z = pbetabinom(y, n, a, b, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
+            y = fmin2(y + incr, size);
+            if(y == size ||
+                    (*z = pbetabinom(y, size, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
                 return y;
 #if DEBUG_c_qbetabinom == 1
 	    printf("\tnew z=%7g >= p = %7g  --> new y=%.0f ..\n", *z,p, y);
@@ -158,77 +158,77 @@ do_search(double y, double *z, double p, double n, double a, double b, double in
     }
 }
 
-double qbetabinom(double p, double n, double a, double b, int lower_tail, int log_p)
+double qbetabinom(double p, double size, double shape1, double shape2, int lower_tail, int log_p)
 {
     double mu, pr, sigma, gamma, z, y;
 
 #ifdef IEEE_754
-    if (ISNAN(p) || ISNAN(n) || ISNAN(a) || ISNAN(b))
-        return p + n + a + b;
+    if (ISNAN(p) || ISNAN(size) || ISNAN(shape1) || ISNAN(shape2))
+        return p + size + shape1 + shape2;
 #endif
-    if(!R_FINITE(n)) return R_NaN;
+    if(!R_FINITE(size)) return R_NaN;
     /* if log_p is true, p = -Inf is a legitimate value */
     if(!R_FINITE(p) && !log_p) return R_NaN;
 
-    if(n != floor(n + 0.5)) return R_NaN;
-    if (a <= 0 || b <= 0 || n < 0) return R_NaN;
+    if(size != floor(size + 0.5)) return R_NaN;
+    if (shape1 <= 0 || shape2 <= 0 || size < 0) return R_NaN;
 
-    R_Q_P01_boundaries(p, 0, n);
+    R_Q_P01_boundaries(p, 0, size);
 
-    pr = a / (a + b); // Beta Mean
-    mu = n * pr; // Mean
-    sigma = sqrt(n * a * b * (n + a + b) / (a + b + 1)) / (b + a); // SD
-    gamma = (a + b + 2 * n) * (b - a) / (a + b + 2) * sqrt((1 + a + b) / (n * a * b * (n + a + b))); // Skewness
+    pr = shape1 / (shape1 + shape2); // Beta Mean
+    mu = size * pr; // Mean
+    sigma = sqrt(size * shape1 * shape2 * (size + shape1 + shape2) / (shape1 + shape2 + 1)) / (shape2 + shape1); // SD
+    gamma = (shape1 + shape2 + 2 * size) * (shape2 - shape1) / (shape1 + shape2 + 2) * sqrt((1 + shape1 + shape2) / (size * shape1 * shape2 * (size + shape1 + shape2))); // Skewness
 
 #if DEBUG_c_qbetabinom == 1
-    printf("qbetabinom(p=%7g, n=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
-             p,n,pr, lower_tail, log_p, sigma, gamma);
+    printf("qbetabinom(p=%7g, size=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
+             p,size,pr, lower_tail, log_p, sigma, gamma);
 #endif   
 #ifdef DEBUG_qbetabinom
-    REprintf("qbetabinom(p=%7g, n=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
-             p,n,pr, lower_tail, log_p, sigma, gamma);
+    REprintf("qbetabinom(p=%7g, size=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
+             p,size,pr, lower_tail, log_p, sigma, gamma);
 #endif
     /* Note : "same" code in qpois.c, qbinom.c, qnbinom.c --
      * FIXME: This is far from optimal [cancellation for p ~= 1, etc]: */
     if(!lower_tail || log_p) {
         p = R_DT_qIv(p); /* need check again (cancellation!): */
         if (p == 0.) return 0.;
-        if (p == 1.) return n;
+        if (p == 1.) return size;
     }
     /* temporary hack --- FIXME --- */
-    if (p + 1.01*DBL_EPSILON >= 1.) return n;
+    if (p + 1.01*DBL_EPSILON >= 1.) return size;
 
     /* y := approx.value (Cornish-Fisher expansion) :  */
     z = qnorm(p, 0., 1., /*lower_tail*/TRUE, /*log_p*/FALSE);
     y = floor(mu + sigma * (z + gamma * (z*z - 1) / 6) + 0.5);
 
-    if(y > n) /* way off */ y = n;
+    if(y > size) /* way off */ y = size;
 #if DEBUG_c_qbetabinom == 1
     printf("  new (p,1-p)=(%7g,%7g), z=qnorm(..)=%7g, y=%5g\n", p, 1-p, z, y);
 #endif
 #ifdef DEBUG_qbetabinom
     REprintf("  new (p,1-p)=(%7g,%7g), z=qnorm(..)=%7g, y=%5g\n", p, 1-p, z, y);
 #endif
-    z = pbetabinom(y, n, a, b, /*lower_tail*/TRUE, /*log_p*/FALSE);
+    z = pbetabinom(y, size, shape1, shape2, /*lower_tail*/TRUE, /*log_p*/FALSE);
 
     /* fuzz to ensure left continuity: */
     p *= 1 - 64*DBL_EPSILON;
 
-    if(n < 1e5) return do_search(y, &z, p, n, a, b, 1);
+    if(size < 1e5) return do_search(y, &z, p, size, shape1, shape2, 1);
     /* Otherwise be a bit cleverer in the search */
     {
-        double incr = floor(n * 0.001), oldincr;
+        double incr = floor(size * 0.001), oldincr;
         do {
             oldincr = incr;
-            y = do_search(y, &z, p, n, a, b, incr);
+            y = do_search(y, &z, p, size, shape1, shape2, incr);
             incr = fmax2(1, floor(incr/100));
-        } while(oldincr > 1 && incr > n*1e-15);
+        } while(oldincr > 1 && incr > size*1e-15);
         return y;
     }
 }
 
 
-double rbetabinom(double n, double a, double b)
+double rbetabinom(double size, double shape1, double shape2)
 {
-    return rbinom(n, rbeta(a, b));
+    return rbinom(size, rbeta(shape1, shape2));
 }
