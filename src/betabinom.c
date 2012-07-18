229c5b111ae69c912ebd1fc016858160bc765234
Joan Maspons <j.maspons@creaf.uab.cat>
2012-07-18 21:28:23 +0200

    Remove Cornish-Fisher Expansion method for quantile functions. It not make sense since cdf function sums the density from 0 to x. Useful if cdf is calculated using 3F2 generalized hypergeometric function and is more efficient.

--- src/betabinom.c
+++ src/betabinom.c
@@ -1,7 +1,7 @@
 /*
  * AUTHOR
- *  2012 Joan Maspons, joanmaspons@gmail.com
- *
+ *  Copyright (C) 2012, Joan Maspons, joanmaspons@gmail.com
+ *  based on binomial distribution code from The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,17 +20,17 @@
  *
  * DESCRIPTION
  *
- *   To compute the beta binomial probability, call dbetabinom(x,n, a, b).
+ *   To compute the beta binomial probability, call dbetabinom(x, size, shape1, shape2).
  *   This checks for argument validity, and calls dbetabinom_raw().
  *
  *   dbetabinom_raw() does the actual computation; note this is called by
  *   other functions in addition to dbetabinom().
  *     (1) dbetabinom_raw() does NOT check that inputs x and n are integers. This
  *         should be done in the calling function, where necessary.
- *     (2) Also does not check for 0 < a and 0 < b or NaN's.
+ *     (2) Also does not check for 0 < shape1 and 0 < shape2 or NaN's.
  *         Do this in the calling function.
  */
-//TODO unify var names a= shape1, b = shape2, n=size
+
 #include <R.h>
 #include <Rmath.h>
 //#include "betaBinom.h"
@@ -69,7 +69,7 @@ double dbetabinom(double x, double size, double shape1, double shape2, int give_
  *
  *  DESCRIPTION
  *
- *    The distribution function of the beta binomial distribution.
+ *    The distribution function of the Beta binomial distribution.
  */
 
 double attribute_hidden pbetabinom_raw(double x, double size, double shape1, double shape2, int log_p)
@@ -100,7 +100,7 @@ double pbetabinom(double x, double size, double shape1, double shape2, int lower
     if (size <= x) return R_DT_1;
 
     double ans = pbetabinom_raw(x, size, shape1, shape2, log_p);
-    if (!lower_tail) ans = 1 - ans;
+    if (!lower_tail) ans = 1 - ans; //TODO check lower_tail -> R_D_Cval
     return ans;
 }
 /*
@@ -108,59 +108,11 @@ double pbetabinom(double x, double size, double shape1, double shape2, int lower
  *  DESCRIPTION
  *
  *	The quantile function of the Beta binomial distribution.
- *
- *  METHOD
- *
- *	Uses the Cornish-Fisher Expansion to include a skewness
- *	correction to a normal approximation. A search is then conducted of values close to
- *	this initial start point.
  */
-
-static double
-do_search(double y, double *z, double p, double size, double shape1, double shape2, double incr)
-{
-    if(*z >= p) {
-        /* search to the left */
-#if DEBUG_c_qbetabinom == 1
-        printf("\tnew z=%7g >= p = %7g  --> search to left (y--) ..\n", *z,p);
-#endif
-#ifdef DEBUG_qbetabinom
-        REprintf("\tnew z=%7g >= p = %7g  --> search to left (y--) ..\n", z,p);
-#endif
-        for(;;) {
-            double newz;
-            if(y == 0 ||
-                    (newz = pbetabinom(y - incr, size, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
-                return y;
-            y = fmax2(0, y - incr);
-            *z = newz;
-#if DEBUG_c_qbetabinom == 1
-	    printf("\tnew z=%7g >= p = %7g  --> new y=%.0f ..\n", *z,p, y);
-#endif    
-        }
-    }
-    else {		/* search to the right */
-#if DEBUG_c_qbetabinom == 1
-        printf("\tnew z=%7g < p = %7g  --> search to right (y++) ..\n", *z,p);
-#endif
-#ifdef DEBUG_qbetabinom
-        REprintf("\tnew z=%7g < p = %7g  --> search to right (y++) ..\n", z,p);
-#endif
-        for(;;) {
-            y = fmin2(y + incr, size);
-            if(y == size ||
-                    (*z = pbetabinom(y, size, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
-                return y;
-#if DEBUG_c_qbetabinom == 1
-	    printf("\tnew z=%7g >= p = %7g  --> new y=%.0f ..\n", *z,p, y);
-#endif 
-        }
-    }
-}
 
 double qbetabinom(double p, double size, double shape1, double shape2, int lower_tail, int log_p)
 {
-    double mu, pr, sigma, gamma, z, y;
+    double pi=0;
 
 #ifdef IEEE_754
     if (ISNAN(p) || ISNAN(size) || ISNAN(shape1) || ISNAN(shape2))
@@ -174,57 +126,12 @@ double qbetabinom(double p, double size, double shape1, double shape2, int lower
     if (shape1 <= 0 || shape2 <= 0 || size < 0) return R_NaN;
 
     R_Q_P01_boundaries(p, 0, size);
-
-    pr = shape1 / (shape1 + shape2); // Beta Mean
-    mu = size * pr; // Mean
-    sigma = sqrt(size * shape1 * shape2 * (size + shape1 + shape2) / (shape1 + shape2 + 1)) / (shape2 + shape1); // SD
-    gamma = (shape1 + shape2 + 2 * size) * (shape2 - shape1) / (shape1 + shape2 + 2) * sqrt((1 + shape1 + shape2) / (size * shape1 * shape2 * (size + shape1 + shape2))); // Skewness
-
-#if DEBUG_c_qbetabinom == 1
-    printf("qbetabinom(p=%7g, size=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
-             p,size,pr, lower_tail, log_p, sigma, gamma);
-#endif   
-#ifdef DEBUG_qbetabinom
-    REprintf("qbetabinom(p=%7g, size=%g, pr=%7g, l.t.=%d, log=%d): sigm=%g, gam=%g\n",
-             p,size,pr, lower_tail, log_p, sigma, gamma);
-#endif
-    /* Note : "same" code in qpois.c, qbinom.c, qnbinom.c --
-     * FIXME: This is far from optimal [cancellation for p ~= 1, etc]: */
-    if(!lower_tail || log_p) {
-        p = R_DT_qIv(p); /* need check again (cancellation!): */
-        if (p == 0.) return 0.;
-        if (p == 1.) return size;
+    
+    for (int i=0; i < size; ++i){
+	pi += dbetabinom_raw(i, size, shape1, shape2, log_p);
+	if (pi > p) return R_D_Cval(i); //TODO check lower_tail
     }
-    /* temporary hack --- FIXME --- */
-    if (p + 1.01*DBL_EPSILON >= 1.) return size;
-
-    /* y := approx.value (Cornish-Fisher expansion) :  */
-    z = qnorm(p, 0., 1., /*lower_tail*/TRUE, /*log_p*/FALSE);
-    y = floor(mu + sigma * (z + gamma * (z*z - 1) / 6) + 0.5);
-
-    if(y > size) /* way off */ y = size;
-#if DEBUG_c_qbetabinom == 1
-    printf("  new (p,1-p)=(%7g,%7g), z=qnorm(..)=%7g, y=%5g\n", p, 1-p, z, y);
-#endif
-#ifdef DEBUG_qbetabinom
-    REprintf("  new (p,1-p)=(%7g,%7g), z=qnorm(..)=%7g, y=%5g\n", p, 1-p, z, y);
-#endif
-    z = pbetabinom(y, size, shape1, shape2, /*lower_tail*/TRUE, /*log_p*/FALSE);
-
-    /* fuzz to ensure left continuity: */
-    p *= 1 - 64*DBL_EPSILON;
-
-    if(size < 1e5) return do_search(y, &z, p, size, shape1, shape2, 1);
-    /* Otherwise be a bit cleverer in the search */
-    {
-        double incr = floor(size * 0.001), oldincr;
-        do {
-            oldincr = incr;
-            y = do_search(y, &z, p, size, shape1, shape2, incr);
-            incr = fmax2(1, floor(incr/100));
-        } while(oldincr > 1 && incr > size*1e-15);
-        return y;
-    }
+    return size;
 }
 
 
--- src/betanbinom.c
+++ src/betanbinom.c
@@ -1,12 +1,7 @@
 /*
- *  AUTHOR
- *    Catherine Loader, catherine@research.bell-labs.com.
- *    October 23, 2000 and Feb, 2001.
- *
- *    dbetanbinom_mu(): Martin Maechler, June 2008
- *
- *  Merge in to R:
- *	Copyright (C) 2000--2008, The R Core Team
+ * AUTHOR
+ *  Copyright (C) 2012, Joan Maspons, joanmaspons@gmail.com
+ *  based on negative binomial distribution code from The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -25,27 +20,31 @@
  *
  * DESCRIPTION
  *
- *   Computes the negative binomial distribution. For integer n,
+ *   Computes the negative binomial distribution. For integer size,
  *   this is probability of x failures before the nth success in a
- *   sequence of Bernoulli trials. We do not enforce integer n, since
+ *   sequence of Bernoulli trials. We do not enforce integer size, since //TODO check statement
  *   the distribution is well defined for non-integers,
  *   and this can be useful for e.g. overdispersed discrete survival times.
  */
+//TODO check alternative parameterization nbinom_mu
 
 #include <R.h>
 #include <Rmath.h>
 #include "betaBinom.h"
 #include "dpq.h"
 #include "R_ext/Visibility.h"
-//TODO
+
 double attribute_hidden dbetanbinom_raw(double x, double size, double shape1, double shape2, int give_log)
 {
     double ans;
  
     if (x < 0 || !R_FINITE(x)) return R_D__0;
     
-    ans = pochhammer(shape1, size) * pochhammer(size, x) * pochhammer(shape2, x) / (gamma(x+1) * pochhammer(shape1 + shape2, size) * pochhammer(size + shape1 + shape2, x)); //factorial(x) = gamma(x+1)
-
+//     ans = pochhammer(shape1, size) * pochhammer(size, x) * pochhammer(shape2, x) / (gamma(x+1) * pochhammer(shape1 + shape2, size) * pochhammer(size + shape1 + shape2, x)); //factorial(x) = gamma(x+1)
+    ans = gammafn(shape1 + size) / gammafn(shape1) * gammafn(size + x) / gammafn(size) * gammafn(shape2 + x) / gammafn(shape2) / (gammafn(x+1) * gammafn(shape1 + shape2 + size) / gammafn(shape1 + shape2) * gammafn(size + shape1 + shape2 + x) / gammafn(size + shape1 + shape2)); //factorial(x) = gamma(x+1)
+if (!R_FINITE(ans)){
+  REprintf("Numerator=%7g Denominator= %7g\n", gammafn(shape1 + size) / gammafn(shape1) * gammafn(size + x) / gammafn(size) * gammafn(shape2 + x) / gammafn(shape2), gammafn(x+1) * gammafn(shape1 + shape2 + size) / gammafn(shape1 + shape2) * gammafn(size + shape1 + shape2 + x) / gammafn(size + shape1 + shape2));
+}
     return R_D_qIv(ans);
 }
 
@@ -63,58 +62,10 @@ double dbetanbinom(double x, double size, double shape1, double shape2, int give
     return dbetanbinom_raw(x, size, shape1, shape2, give_log);
 }
 
-// double dnbinom_mu(double x, double size, double mu, int give_log)
-// {
-//     /* originally, just set  prob :=  size / (size + mu)  and called dbinom_raw(),
-//      * but that suffers from cancellation when   mu << size  */
-//     double ans, p;
-// 
-// #ifdef IEEE_754
-//     if (ISNAN(x) || ISNAN(size) || ISNAN(mu))
-//         return x + size + mu;
-// #endif
-// 
-//     if (mu < 0 || size < 0) ML_ERR_return_NAN;
-//     R_D_nonint_check(x);
-//     if (x < 0 || !R_FINITE(x)) return R_D__0;
-//     x = R_D_forceint(x);
-//     if(x == 0)/* be accurate, both for n << mu, and n >> mu :*/
-// 	return R_D_exp(size * (size < mu ? log(size/(size+mu)) : log1p(- mu/(size+mu))));
-//     if(x < 1e-10 * size) { /* don't use dbinom_raw() but MM's formula: */
-// 	/* FIXME --- 1e-8 shows problem; rather use algdiv() from ./toms708.c */
-// 	return R_D_exp(x * log(size*mu / (size+mu)) - mu - lgamma(x+1) +
-// 		       log1p(x*(x-1)/(2*size)));
-//     }
-//     /* else: no unnecessary cancellation inside dbinom_raw, when
-//      * x_ = size and n_ = x+size are so close that n_ - x_ loses accuracy
-//      */
-//     ans = dbinom_raw(size, x+size, size/(size+mu), mu/(size+mu), give_log);
-//     p = ((double)size)/(size+x);
-//     return((give_log) ? log(p) + ans : p * ans);
-// }
-
 /*
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-8 The R Core Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, a copy is available at
- *  http://www.r-project.org/Licenses/
- *
  *  DESCRIPTION
  *
- *	The distribution function of the negative binomial distribution.
+ *	The distribution function of the Beta negative binomial distribution.
  *
  *  NOTES
  *
@@ -145,68 +96,14 @@ double pbetanbinom(double x, double size, double shape1, double shape2, int lowe
     x = floor(x + 1e-7);
     
     double ans = pbetanbinom_raw(x, size, shape1, shape2, log_p);
-    if (!lower_tail) ans = 1 - ans;
+    if (!lower_tail) ans = 1 - ans; //TODO check lower_tail -> R_D_Cval
     return ans;
 }
 
-// double pbetanbinom_mu(double x, double size, double mu, int lower_tail, int log_p)
-// {
-// #ifdef IEEE_754
-//     if (ISNAN(x) || ISNAN(size) || ISNAN(mu))
-// 	return x + size + mu;
-//     if(!R_FINITE(size) || !R_FINITE(mu))	ML_ERR_return_NAN;
-// #endif
-//     if (size <= 0 || mu < 0)	ML_ERR_return_NAN;
-// 
-//     if (x < 0) return R_DT_0;
-//     if (!R_FINITE(x)) return R_DT_1;
-//     x = floor(x + 1e-7);
-//     /* return
-//      * pbeta(pr, size, x + 1, lower_tail, log_p);  pr = size/(size + mu), 1-pr = mu/(size+mu)
-//      *
-//      *= pbeta_raw(pr, size, x + 1, lower_tail, log_p)
-//      *            x.  pin   qin
-//      *=  bratio (pin,  qin, x., 1-x., &w, &wc, &ierr, log_p),  and return w or wc ..
-//      *=  bratio (size, x+1, pr, 1-pr, &w, &wc, &ierr, log_p) */
-//     {
-// 	int ierr;
-// 	double w, wc;
-// 	bratio(size, x+1, size/(size+mu), mu/(size+mu), &w, &wc, &ierr, log_p);
-// 	if(ierr)
-// 	    MATHLIB_WARNING(_("pbetanbinom_mu() -> bratio() gave error code %d"), ierr);
-// 	return lower_tail ? w : wc;
-//     }
-// }
-
 /*
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000-2008 The R Core Team
- *  Copyright (C) 2005 The R Foundation
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, a copy is available at
- *  http://www.r-project.org/Licenses/
- *
- *  SYNOPSIS
- *
- *	#include <Rmath.h>
- *	double qbetanbinom(double p, double size, double shape1, double shape2,
- *                     int lower_tail, int log_p)
- *
  *  DESCRIPTION
  *
- *	The quantile function of the negative binomial distribution.
+ *	The quantile function of the Beta negative binomial distribution.
  *
  *  NOTES
  *
@@ -221,112 +118,28 @@ double pbetanbinom(double x, double size, double shape1, double shape2, int lowe
  *	this initial start point.
  */
 
-static double
-do_search(double y, double *z, double p, double n, double shape1, double shape2, double incr)
-{
-    if(*z >= p) {
-			/* search to the left */
-	for(;;) {
-	    if(y == 0 ||
-	       (*z = pbetanbinom(y - incr, n, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
-		return y;
-	    y = fmax2(0, y - incr);
-	}
-    }
-    else {		/* search to the right */
-
-	for(;;) {
-	    y = y + incr;
-	    if((*z = pbetanbinom(y, n, shape1, shape2, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
-		return y;
-	}
-    }
-}
-
-
 double qbetanbinom(double p, double size, double shape1, double shape2, int lower_tail, int log_p)
 {
-    double P, Q, mu, sigma, gamma, z, y;
+    double pi, i = 0;
 
 #ifdef IEEE_754
     if (ISNAN(p) || ISNAN(size) || ISNAN(shape1) || ISNAN(shape2))
 	return p + size + shape1 + shape2;
 #endif
-    if (shape1 <= 0 || shape2 <= 0|| size <= 0) return R_NaN;
-    /* FIXME: size = 0 is well defined ! */
+    if (shape1 <= 0 || shape2 <= 0|| size < 0) return R_NaN;
 
     R_Q_P01_boundaries(p, 0, R_PosInf);
-
-    double prob =  shape1 <= 1? R_PosInf : shape2 / (shape1 - 1); //TODO alternative method for shape1 <= 3
-    Q = 1.0 / prob;
-    P = (1.0 - prob) * Q;
-    mu = size * prob;
-    sigma = sqrt(size * (shape1 + size - 1) * shape2 * (shape1 + shape2 - 1) / ((shape1 - 2) * R_pow_di(shape1 - 1, 2))); //SD
-    gamma = (shape1 + 2 * size - 1) * (shape1 + 2 * shape2 - 1) / ((shape1 - 3) * sqrt(size * (shape1 + size - 1) * shape2 * (shape1 + shape2 - 1) / (shape1 - 2))); //Skewsizeess
-
-    /* Note : "same" code in qpois.c, qbinom.c, qnbinom.c --
-     * FIXME: This is far from optimal [cancellation for p ~= 1, etc]: */
-    if(!lower_tail || log_p) {
-	p = R_DT_qIv(p); /* need check again (cancellation!): */
-	if (p == R_DT_0) return 0;
-	if (p == R_DT_1) return R_PosInf;
+    
+    
+    while (pi < p) {
+	pi += dbetanbinom_raw(i, size, shape1, shape2, log_p);
+	i++;
     }
-    /* temporary hack --- FIXME --- */
-    if (p + 1.01*DBL_EPSILON >= 1.) return R_PosInf;
-
-    /* y := approx.value (Cornish-Fisher expansion) :  */
-    z = qnorm(p, 0., 1., /*lower_tail*/TRUE, /*log_p*/FALSE);
-    y = floor(mu + sigma * (z + gamma * (z*z - 1) / 6) + 0.5);
-
-    z = pbetanbinom(y, size, shape1, shape2, /*lower_tail*/TRUE, /*log_p*/FALSE);
-
-    /* fuzz to ensure left continuity: */
-    p *= 1 - 64*DBL_EPSILON;
-
-    /* If the C-F value is not too large a simple search is OK */
-    if(y < 1e5) return do_search(y, &z, p, size, shape1, shape2, 1);
-    /* Otherwise be a bit cleverer in the search */
-    {
-	double incr = floor(y * 0.001), oldincr;
-	do {
-	    oldincr = incr;
-	    y = do_search(y, &z, p, size, shape1, shape2, incr);
-	    incr = fmax2(1, floor(incr/100));
-	} while(oldincr > 1 && incr > y*1e-15);
-	return y;
-    }
+    
+    return i; //TODO check lower_tail -> R_D_Cval
 }
 
-// double qbetanbinom_mu(double p, double size, double mu, int lower_tail, int log_p)
-// {
-// /* FIXME!  Implement properly!! (not losing accuracy for very large size (prob ~= 1)*/
-//     return qbetanbinom(p, size, /* prob = */ size/(size+mu), lower_tail, log_p);
-// }
-
 /*
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000--2006  The R Core Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, a copy is available at
- *  http://www.r-project.org/Licenses/
- *
- *  SYNOPSIS
- *
- *    #include <Rmath.h>
- *    double rbetanbinom(double n, double p)
- *
  *  DESCRIPTION
  *
  *    Random variates from the negative binomial distribution.
@@ -354,13 +167,6 @@ double rbetanbinom(double size, double shape1, double shape2)
 // 	return R_NaN;
     return rnbinom(size, rbeta(shape1, shape2));
 }
-
-// double rbetanbinom_mu(double size, double mu)
-// {
-//     if(!R_FINITE(size) || !R_FINITE(mu) || size <= 0 || mu < 0)
-// 	ML_ERR_return_NAN;
-//     return (mu == 0) ? 0 : rpois(rgamma(size, mu / size));
-// }
 
 /*
 MeanBetaBinomialNegativaDist<- function(n, a, b){ # a i b són paràmetres Beta
