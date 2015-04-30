7d48f09d667e6a9dddf64ce6ce1c3ff1ad8a1ab6
Joan Maspons <j.maspons@creaf.uab.cat>
2015-04-30 16:43:22 +0200

    Update tests for Environment class

--- test/Environment.r
+++ test/Environment.r
@@ -1,18 +1,29 @@
 # Constructor
-Environment(1)
-Environment(.5, var=.1)
-Environment(.5, seasonAmplitude=.3)
-Environment(seasonRange=c(0,1))
-Environment(.5, var=.1, seasonAmplitude=.3)
-Environment(.5, var=1) # parameters out of the Beta distribution domain
-Environment(.5, seasonRange=c(0,1)) # mean and range are redundant parameters on a sinusoidal function
+env<- Env()
+Env(.5, var=.1)
+Env(.5, seasonAmplitude=.3)
+Env(seasonRange=c(0,1))
+Env(.5, var=.1, seasonAmplitude=.3)
+Env(.5, var=1) # parameters out of the Beta distribution domain
+Env(.5, seasonRange=c(0,1)) # mean and range are redundant parameters on a sinusoidal function
 
 ## Seasonal pattern
-seasonalPattern(Environment(.5, seasonAmplitude=1))
-seasonalPattern(Environment(.5, seasonAmplitude=1), 365)
-seasonalPattern(Environment(.5, seasonAmplitude=1), nSteps=3, interval=1)
+seasonalPattern(env)
+seasonalPattern(env, resolution=365)
+seasonalPattern(env, cicles=2)
 
-seasonalPattern(Environment(.5, seasonAmplitude=1), nSteps=3, interval=1)
-seasonalPattern(Environment(.5, seasonAmplitude=1), nSteps=3, interval=1, criterion="maxMean")
-seasonalPattern(Environment(.5, seasonAmplitude=1), nSteps=3, interval=2, criterion="maxMean")
-seasonalPattern(Environment(.5, seasonAmplitude=1), nSteps=4, interval=2, criterion="maxMean")
+seasonOptimCal(env)
+seasonOptimCal(env, resolution=12, nSteps=3, interval=1, criterion="maxFirst")
+seasonOptimCal(env, resolution=12, nSteps=3, interval=3, criterion="maxFirst")
+seasonOptimCal(env, resolution=12, nSteps=3, interval=1, criterion="maxMean")
+seasonOptimCal(env, resolution=12, nSteps=3, interval=3, criterion="maxMean")
+
+# # nSteps<- c(1,3, 5)
+# # interval<- c(1,3)
+# # criterion<- c("maxFirst", "maxMean")
+# # cals<- expand.grid(nSteps, interval, criterion, stringsAsFactors=FALSE)
+# # names(cals)<- c("nSteps", "interval", "criterion")
+# # cals$resolution<- 12
+# #
+# # envCals<- merge(S3Part(env), cals)
+# # seasonOptimCal(envCals)
