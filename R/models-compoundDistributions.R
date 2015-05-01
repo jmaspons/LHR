0e5fd70b290f43548f73034348d8793c4f01d93d
Joan Maspons <j.maspons@creaf.uab.cat>
2015-05-01 10:29:04 +0200

    Revert "Update functions to work with a object of class LH as a parameter".
    Better to keep it simple and add wrappers to Model class to run different models according to the parameters.

--- R/models-compoundDistributions.R
+++ R/models-compoundDistributions.R
@@ -2,34 +2,30 @@
 
 # Adult mortality + offspring mortality
 # N_t+1 = B(n=N_t * fecundity, p=j) + B(n=N_t, p=a)
-mFit.trans<- function(LH, N){
-  N_t1<- with(LH, expr={
-    ## Recruitment probability conditioned to the number of nest success
-    recruits<- distriBinom(N * fecundity, prob=j)
-    
-    ## Adult survival
-    survivors<- distriBinom(size=N, prob=a)
+mFit.trans<- function(fecundity, j, a, N){
+  ## Recruitment probability conditioned to the number of nest success
+  recruits<- distriBinom(N * fecundity, prob=j)
   
-    distriSum(recruits, survivors)
-  })
+  ## Adult survival
+  survivors<- distriBinom(size=N, prob=a)
+  N_t1<- distriSum(recruits, survivors)
+
   return (N_t1)
 }
 
 # Adult mortality + Nest mortality + offspring mortality
 # N_t+1 = B(n=B(n=N_t * broods, p=1-nestFail) * b, p=j) + B(n=N_t, p=a)
-mSurvBV.trans<- function(LH, nestFail, N){
-  LH$j<- nestFail / LH$j
-  N_t1<- with(LH, expr={
-    ## Probability of successful nesting without complete breeding fail
-    nSuccessBroods<- with(LH, distriBinom(N * broods, prob=1-nestFail))
-    nSuccessBroods$x<- nSuccessBroods$x * b
-    ## Recruitment probability conditioned to the number of nest success
-    recruits<- distriBinom(nSuccessBroods, prob=LH$j)
-    
-    ## Adult survival
-    survivors<- distriBinom(size=N, prob=a)
-    N_t1<- distriSum(recruits, survivors)
-  })
+mSurvBV.trans<- function(broods, b, j, a, nestFail, N){
+  ## Probability of successful nesting without complete breeding fail
+  nSuccessBroods<- distriBinom(N * broods, prob=1-nestFail)
+  nSuccessBroods$x<- nSuccessBroods$x * b
+  ## Recruitment probability conditioned to the number of nest success
+  recruits<- distriBinom(nSuccessBroods, prob=j)
+  
+  ## Adult survival
+  survivors<- distriBinom(size=N, prob=a)
+  N_t1<- distriSum(recruits, survivors)
+
   return (N_t1)
 }
 
@@ -38,33 +34,30 @@ mSurvBV.trans<- function(LH, nestFail, N){
 
 # Adult mortality + offspring mortality + sex ratio
 # N_t+1 = B(n=B(n=N_t * fecundity, p=j), p=sexRatio) + B(n=N_t, p=a)
-mFitSex.trans<- function(LH, sexRatio=0.5, Nf, Nm, matingSystem=c("monogamy", "polygyny", "polyandry")[1]){
-  N_t1<- with(LH, expr={
-    ## Recruitment probability conditioned to the number of nest success
-    N<- switch(matingSystem, 
-                    monogamy=min(Nf, Nm),
-                    polygyny=Nf,
-                    polyandry=Nm)
-    recruits<- distriBinom(N * fecundity, prob=j)
-    recruitsF<- distriBinom(recruits, prob=sexRatio)
-    recruitsM<- recruits - recruitsF
+mFitSex.trans<- function(fecundity, j, a, sexRatio=0.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], Nf, Nm=Nf){
+  ## Recruitment probability conditioned to the number of nest success
+  N<- switch(matingSystem, 
+                  monogamy=min(Nf, Nm),
+                  polygyny=Nf,
+                  polyandry=Nm)
+  recruits<- distriBinom(N * fecundity, prob=j)
+  recruitsF<- distriBinom(recruits, prob=sexRatio)
+  recruitsM<- recruits - recruitsF
+  
+  ## Adult survival
+  survivorsF<- distriBinom(size=Nf, prob=a)
+  survivorsM<- distriBinom(size=Nm, prob=a)
+  Nf_t1<- distriSum(recruitsF, survivorsF)
+  Nm_t1<- distriSum(recruitsF, survivorsM)
+  N_t1<- list(females=Nf_t1, males=Nm_t1)
     
-    ## Adult survival
-    survivorsF<- distriBinom(size=Nf, prob=a)
-    survivorsM<- distriBinom(size=NM, prob=a)
-    Nf_t1<- distriSum(recruitsF, survivorsF)
-    Nm_t1<- distriSum(recruitsF, survivorsM)
-  })
   return (N_t1)
 }
 
 # Adult mortality + Nest mortality + offspring mortality + sex ratio
 # N_t+1 = B(n=B(n=B(n=N_t * broods, p=1-nestFail) * b, p=j), p=sexRatio) + B(n=N_t, p=a)
-mSurvBVSex.trans<- function(LH, nestFail, sexRatio=0.5, Nf, Nm){
-  LH$j<- nestFail / LH$j
-  N_t1<- with(LH, expr={  
-    
-  })
+mSurvBVSex.trans<- function(broods, b, j, a, nestFail, sexRatio=0.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], Nf, Nm){
+
 }
 
 
@@ -73,42 +66,37 @@ mSurvBVSex.trans<- function(LH, nestFail, sexRatio=0.5, Nf, Nm){
 
 # Nest mortality + offspring mortality
 # cohort fitness = B(B(n=broods * lifespan, p=1-nestFail) * b, p=jusSurv)
-mBV.cohort<- function(LH, lifespan, nestFail){
-  LH$j<- nestFail / LH$j
-  N_t1<- with(LH, expr={
-    ## Probability of successful nesting without complete breeding fail
-    nSuccessBroods<- distriBinom(broods * lifespan, prob=1-nestFail)
-    # Multiply the number of successful broods by b size
-    nSuccessBroods$x<- nSuccessBroods$x * b
-    ## Fitness probability conditioned to the number of nest success
-    fitness<- distriBinom(nSuccessBroods, prob=j)
-  })
+mBV.cohort<- function(j, lifespan, nestFail){
+  ## Probability of successful nesting without complete breeding fail
+  nSuccessBroods<- distriBinom(broods * lifespan, prob=1-nestFail)
+  # Multiply the number of successful broods by b size
+  nSuccessBroods$x<- nSuccessBroods$x * b
+  ## Fitness probability conditioned to the number of nest success
+  fitness<- distriBinom(nSuccessBroods, prob=j)
+
   return (fitness)
 }
 
 # Adult mortality + offspring mortality
 # cohort fitness = B(n=NB(n=N_0, p=1-a) * fecundity, p=j)
-mFit.cohort<- function(LH, N0=1, ...){ # ... = maxX or p.omited 
-  N_t1<- with(LH, expr={
-    lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
-    fitness<- distriBinom(size=fecundity, prob=j)
-  })
+mFit.cohort<- function(fecundity, j, a, N0=1, ...){ # ... = maxX or p.omited 
+  lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
+  fitness<- distriBinom(size=fecundity, prob=j)
+
   return (fitness)
 }
 
 # Adult mortality + Nest mortality + offspring mortality
 # cohort fitness = B(n=B(n=NB(n=N_0, p=1-a) * broods, p=1-nestFail) * b, p=j)
-mSurvBV.cohort<- function(LH, nestFail, N0=1, ...){ # ... = maxX or p.omited
-  LH$j<- nestFail / LH$j
-  N_t1<- with(LH, expr={
-    lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
-    lifespan$x<- lifespan$x * broods # Number of broods for the whole life
-    ## Probability of successful nesting without complete breeding fail
-    nSuccessBroods<- distriBinom(lifespan, prob=1-nestFail)
-    nSuccessBroods$x<- nSuccessBroods$x * b
-    ## Fitness probability conditioned to the number of nest success
-    fitness<- distriBinom(nSuccessBroods, prob=j)
-  })
+mSurvBV.cohort<- function(broods, b, j, a, nestFail, N0=1, ...){ # ... = maxX or p.omited
+  lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
+  lifespan$x<- lifespan$x * broods # Number of broods for the whole life
+  ## Probability of successful nesting without complete breeding fail
+  nSuccessBroods<- distriBinom(lifespan, prob=1-nestFail)
+  nSuccessBroods$x<- nSuccessBroods$x * b
+  ## Fitness probability conditioned to the number of nest success
+  fitness<- distriBinom(nSuccessBroods, prob=j)
+
   return (fitness)
 }
 
@@ -117,32 +105,15 @@ mSurvBV.cohort<- function(LH, nestFail, N0=1, ...){ # ... = maxX or p.omited
 
 # Adult mortality + offspring mortality + sex ratio
 # fitness = B(n=B(n=NB(n=N_0, p=1-a) * fecundity, p=j), p=sexRatio)
-mFitSex.cohort<- function(LH, sexRatio=0.5, N0=1, ...){ # ... = maxX or p.omited 
-  N_t1<- with(LH, expr={
-    lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
-    fitness<- distriBinom(size=fecundity, prob=j)
-    females<- distriBinom(size=fitness, prob=sexRatio)
-    males<- distriBinom(size=fitness, prob=1-sexRatio)
-  })
-  return (list(females=females, males=males))
-}
+# mFitSex.cohort<- function(fecundity, j, a, sexRatio=0.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], Nf, Nm=Nf, ...){ # ... = maxX or p.omited 
+#     lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
+#     fitness<- distriBinom(size=fecundity, prob=j)
+#     females<- distriBinom(size=fitness, prob=sexRatio)
+#     males<- distriBinom(size=fitness, prob=1-sexRatio)
+# 
+#   return (list(females=females, males=males))
+# }
 
 # Adult mortality + Nest mortality + offspring mortality + sex ratio
 # fitness = B(n=B(n=B(n=NB(n=N_0, p=1-a) * broods, p=1-nestFail) * b, p=j), p=sexRatio)
-mSurvBVSex.cohort<- function(LH, nestFail, sexRatio=0.5, N0=1, ...){ # ... = maxX or p.omited 
-  LH$j<- nestFail / LH$j
-  N_t1<- with(LH, expr={
-    lifespan<- distriNegBinom(size=N0, prob=1-a, ...)
-    lifespan$x<- lifespan$x * broods # Number of broods for the whole life
-    ## Probability of successful nesting without complete breeding fail
-    nSuccessBroods<- distriBinom(lifespan, prob=1-nestFail)
-    nSuccessBroods$x<- nSuccessBroods$x * b
-    ## Fitness probability conditioned to the number of nest success
-    fitness<- distriBinom(nSuccessBroods, prob=j)
-    
-    females<- distriBinom(size=fitness, prob=sexRatio)
-    males<- distriBinom(size=fitness, prob=1-sexRatio)
-  })
-  return (list(females=females, males=males))
-}
 
--- R/models-discreteTime.R
+++ R/models-discreteTime.R
@@ -5,9 +5,9 @@ setOldClass("discretePopSim")
 # When the population gets extinct it fills results with NAs.
 extinctNA<- function(pop){
   extT<- apply(pop, 1, function(x) match(0, x))
-  extT<- extT[extT < ncol(pop)]
+  extT[extT == ncol(pop)]<- NA 
   extPop<- which(!is.na(extT))
-  for (i in seq_along(extPop)){
+  for (i in extPop){
     pop[i,(extT[i]+1):ncol(pop)]<- NA
   }
   return(pop)
@@ -18,55 +18,43 @@ extinctNA<- function(pop){
 # N_t+1 = B(n=NB(n=N_t, p=1-a) * fecundity, p=j)
 # Juveniles reach adult stage in one time step (age at first reproduction = 1)
 # fecundity = b x broods
-mFit.t<- function(LH, N0, replicates, tf, maxN=100000){
-  pop<- with(LH, expr={
-    pop<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
-    pop[,1]<- N0
-    for (t in 1:tf){
-      # Juvenile survivors
-      pop[,t+1]<- rbinom(replicates, pop[,t] * fecundity, j)
-      # Add adult survivors
-      pop[,t+1]<- pop[,t+1] + rbinom(replicates, pop[,t], a)
-      pop[which(pop[,t+1] > maxN),t+1]<- maxN
-    }
-    pop<- pop[order(pop[,tf+1]),]
-    pop<- extinctNA(pop)
-    pop<- as.data.frame(pop)
-    class(pop)<- c("discretePopSim", "data.frame")
-    pop
-  })
+mFit.t<- function(fecundity, j, a, N0, replicates, tf, maxN=100000){
+  pop<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
+  pop[,1]<- N0
+  for (t in 1:tf){
+    # Juvenile survivors
+    pop[,t+1]<- rbinom(replicates, pop[,t] * fecundity, j)
+    # Add adult survivors
+    pop[,t+1]<- pop[,t+1] + rbinom(replicates, pop[,t], a)
+    pop[which(pop[,t+1] > maxN),t+1]<- maxN
+  }
+  pop<- pop[order(pop[,tf+1]),]
+  pop<- extinctNA(pop)
+  pop<- as.data.frame(pop)
+  class(pop)<- c("discretePopSim", "data.frame")
+  
   return(pop)
 }
 
 # Adult mortality + Nest mortality + offspring mortality
 # N_t+1 = B(n=B(n=NB(n=N_t, p=1-a) * broods, p=1-nestFail) * b, p=j)
 # Juveniles reach adult stage in one time step (age at first reproduction = 1)
-mSurvBV.t<- function(LH, nestFail, N0, replicates, tf, maxN=100000){
-  LH$j<- LH$j / (1 - nestFail)
-#   to-report annual2monthMortality [p]      
-#   annualSurv = monthlySurv ^ 12    =>   monthlySurv = anualSurv ^ 1 / 12
-#   monthSurv = (1 - annualMortality) ^ (1 / 12)
-#   negative binomial: annualSurv = pnbinom(q=0, size=12, prob=monthlySurv) => 0 fails en 12 trails
-#   report 1 - (1 - p) ^ (1 / 12)
-  
-  
-  pop<- with(LH, expr={
-    pop<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
-    pop[,1]<- N0
-    for (t in 1:tf){
-      succeedingBroods<- rbinom(replicates, pop[,t] * broods, 1 - nestFail)
-      # Juvenile survivors
-      pop[,t+1]<- rbinom(replicates, b * succeedingBroods, j)
-      # Add adult survivors
-      pop[,t+1]<- pop[,t+1] + rbinom(replicates, pop[,t], a)
-      pop[which(pop[,t+1] > maxN),t+1]<- maxN
-    }
-    pop<- pop[order(pop[,tf+1]),]
-    pop<- extinctNA(pop)
-    pop<- as.data.frame(pop)
-    class(pop)<- c("discretePopSim", "data.frame")
-    pop
-  })
+mSurvBV.t<- function(broods, b, j, a, nestFail, N0, replicates, tf, maxN=100000){
+  pop<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
+  pop[,1]<- N0
+  for (t in 1:tf){
+    succeedingBroods<- rbinom(replicates, pop[,t] * broods, 1 - nestFail)
+    # Juvenile survivors
+    pop[,t+1]<- rbinom(replicates, b * succeedingBroods, j)
+    # Add adult survivors
+    pop[,t+1]<- pop[,t+1] + rbinom(replicates, pop[,t], a)
+    pop[which(pop[,t+1] > maxN),t+1]<- maxN
+  }
+  pop<- pop[order(pop[,tf+1]),]
+  pop<- extinctNA(pop)
+  pop<- as.data.frame(pop)
+  class(pop)<- c("discretePopSim", "data.frame")
+
   return(pop)
 }
 
@@ -76,68 +64,65 @@ mSurvBV.t<- function(LH, nestFail, N0, replicates, tf, maxN=100000){
 # Juveniles reach adult stage in one time step (age at first reproduction = 1)
 # fecundity = b x broods
 ## TODO:
-mFitSex.t<- function(LH, sexRatio=.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], N0, replicates, tf, maxN=100000){
-  pops<- with(LH, expr={
-    popF<- popM<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
-    popF[,1]<- popM[,1]<- N0
-    for (t in 1:tf){
-      nPairs<- switch(matingSystem, 
-                      monogamy=min(popF[,t], popM[,t]),
-                      polygyny=popF[,t],
-                      polyandry=popM[,t])
-      # Juvenile survivors
-      reclutes<- rbinom(replicates, nPairs * fecundity, j)
-      # Sex ratio
-      popF[,t+1]<- rbinom(replicates, reclutes, sexRatio)
-      popM[,t+1]<- reclutes - popF[,t+1]
-      # Add adult survivors
-      popF[,t+1]<- popF[,t+1] + rbinom(replicates, popF[,t], a)
-      popM[,t+1]<- popM[,t+1] + rbinom(replicates, popM[,t], a)
-      popF[which(popF[,t+1] > maxN),t+1]<- maxN
-      popM[which(popM[,t+1] > maxN),t+1]<- maxN
-    }
-    popF<- extinctNA(popF)
-    popM<- extinctNA(popM)
-    popF<- as.data.frame(popF)
-    popM<- as.data.frame(popM)
-    class(popF)<- class(popM)<- c("discretePopSim", "data.frame")
-    pops<- list(females=popF, males=popM)
-  })
+mFitSex.t<- function(fecundity, j, a, sexRatio=.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], N0, replicates, tf, maxN=100000){
+  popF<- popM<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
+  popF[,1]<- popM[,1]<- N0
+  for (t in 1:tf){
+    nPairs<- switch(matingSystem, 
+                    monogamy=min(popF[,t], popM[,t]),
+                    polygyny=popF[,t],
+                    polyandry=popM[,t])
+    # Juvenile survivors
+    reclutes<- rbinom(replicates, nPairs * fecundity, j)
+    # Sex ratio
+    popF[,t+1]<- rbinom(replicates, reclutes, sexRatio)
+    popM[,t+1]<- reclutes - popF[,t+1]
+    # Add adult survivors
+    popF[,t+1]<- popF[,t+1] + rbinom(replicates, popF[,t], a)
+    popM[,t+1]<- popM[,t+1] + rbinom(replicates, popM[,t], a)
+    popF[which(popF[,t+1] > maxN),t+1]<- maxN
+    popM[which(popM[,t+1] > maxN),t+1]<- maxN
+  }
+  popF<- extinctNA(popF)
+  popM<- extinctNA(popM)
+  popF<- as.data.frame(popF)
+  popM<- as.data.frame(popM)
+  class(popF)<- class(popM)<- c("discretePopSim", "data.frame")
+  pops<- list(females=popF, males=popM)
+
   return(pops)
 }
 
 # Adult mortality + Nest mortality + offspring mortality + sex ratio
 # N_t+1 = B(n=B(n=B(n=NB(n=N_t, p=1-a) * broods, p=1-nestFail) * b, p=j), p=sexRatio)
 # Juveniles reach adult stage in one time step (age at first reproduction = 1)
-mSurvBVSex.t<- function(LH, nestFail, sexRatio=.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], N0, replicates, tf, maxN=100000){
-  LH$j<- LH$j / (1 - nestFail)
-  pops<- with(LH, expr={
-    popF<- popM<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
-    popF[,1]<- popM[,1]<- N0
-    for (t in 1:tf){
-      nPairs<- switch(matingSystem, 
-                       monogamy=min(popF[,t], popM[,t]),
-                       polygyny=popF[,t],
-                       polyandry=popM[,t])
-      succeedingBroods<- rbinom(replicates, nPairs * broods, 1 - nestFail)
-      # Juvenile survivors
-      reclutes<- rbinom(replicates, b * succeedingBroods, j)
-      # Sex ratio
-      popF[,t+1]<- rbinom(replicates, reclutes, sexRatio)
-      popM[,t+1]<- reclutes - popF[,t+1]
-      # Add adult survivors
-      popF[,t+1]<- popF[,t+1] + rbinom(replicates, popF[,t], a)
-      popM[,t+1]<- popM[,t+1] + rbinom(replicates, popM[,t], a)
-      popF[which(popF[,t+1] > maxN),t+1]<- maxN
-      popM[which(popM[,t+1] > maxN),t+1]<- maxN
-    }
-    popF<- extinctNA(popF)
-    popM<- extinctNA(popM)
-    popF<- as.data.frame(popF)
-    popM<- as.data.frame(popM)
-    class(popF)<- class(popM)<- c("discretePopSim", "data.frame")
-    pops<- list(females=popF, males=popM)
-  })
+mSurvBVSex.t<- function(broods, b, j, a, nestFail, sexRatio=.5, matingSystem=c("monogamy", "polygyny", "polyandry")[1], N0, replicates, tf, maxN=100000){
+  popF<- popM<- matrix(NA, replicates, tf+1, dimnames=list(replicate=NULL, t=0:tf))
+  popF[,1]<- popM[,1]<- N0
+  for (t in 1:tf){
+    nPairs<- switch(matingSystem, 
+                     monogamy=min(popF[,t], popM[,t]),
+                     polygyny=popF[,t],
+                     polyandry=popM[,t])
+    succeedingBroods<- rbinom(replicates, nPairs * broods, 1 - nestFail)
+    # Juvenile survivors
+    reclutes<- rbinom(replicates, b * succeedingBroods, j)
+    # Sex ratio
+    popF[,t+1]<- rbinom(replicates, reclutes, sexRatio)
+    popM[,t+1]<- reclutes - popF[,t+1]
+    # Add adult survivors
+    popF[,t+1]<- popF[,t+1] + rbinom(replicates, popF[,t], a)
+    popM[,t+1]<- popM[,t+1] + rbinom(replicates, popM[,t], a)
+    popF[which(popF[,t+1] > maxN),t+1]<- maxN
+    popM[which(popM[,t+1] > maxN),t+1]<- maxN
+  }
+  popF<- extinctNA(popF)
+  popM<- extinctNA(popM)
+  popF<- as.data.frame(popF)
+  popM<- as.data.frame(popM)
+  class(popF)<- class(popM)<- c("discretePopSim", "data.frame")
+  pops<- list(females=popF, males=popM)
+
   return(pops)
 }
 
