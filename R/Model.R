b720b5e23f32615c517b1c80768e207da12d4777
Joan Maspons <j.maspons@creaf.uab.cat>
2015-06-17 12:23:13 +0200

    Split juvenile survival (j) in brood level survival (jbr) and individual level survival (jind). breedFail is the proportion of the mortality occurring at brood level

--- R/Model.R
+++ R/Model.R
@@ -13,8 +13,6 @@ setMethod("Model_complete",
             
             model<- new("Model", 
                         scenario,
-                        #                         lh=lh, Data in scenario and easily to recreate the obj with LH(unique(scenario))
-                        #                         env=env,
                         sim=sim,
                         params=parameters)
             return (model)
@@ -48,24 +46,15 @@ setMethod("combineLH_Env",
             scenario<- merge(S3Part(lh), S3Part(env))
             
             ## Brood failure (e.g. nest predation)
-            # breed failure is a proportion of juvenile mortality correlated at the brood level j = jEnv * breedFailLH
-            #  j is survival, and breed failure a mortality
-            scenario$jM<- 1 - (1 - scenario$j) * (1 - scenario$breedFail)  # juvenile mortality is divided in brood mortality and individual mortality
-            scenario$breedFailM<- (1 - scenario$j) * scenario$breedFail
+            # breedFail is a proportion of juvenile mortality correlated at the brood level
+            # survival: P(j) = P(jbr ∩ jind) = P(jbr) * P(jind)
+            # death:    P(-j) = P(-jbr ∪ P(-jind | jbr))
+            scenario$jind<- scenario$j / (scenario$breedFail * (scenario$j-1) + 1)     # juvenile mortality is divided in brood mortality and individual mortality
+            scenario$jbr<- scenario$breedFail * (scenario$j-1) + 1
 
-#             sel<- which(scenario$breedFail == 1)
-#             scenario$jM[sel]<- 1 # corrects /0.  When breedFail == 1 all juvenile mortality is from breeding fail and there is no individual mortality
-#             scenario$breedFailM<- scenario$j * scenario$breedFail
-            
             ## Seasonality
             seasonBroods<- data.frame(unique(scenario[,c("mean", "seasonAmplitude", "broods")]), var=0, breedFail=0) # var necessary for Env(seasonBroods)
             seasonVar<- seasonOptimCal(env=Env(seasonBroods), resolution=resolution, nSteps=seasonBroods$broods, interval=interval, criterion=criterion)
-            
-            ##Stochasticity
-            # Calculated later when running simulations
-#             betaPars<- unique(scenario[,c("mean", "var", "a", "s", "j", "jEnv")])
-#             fbeta(mean=betaPars$mean * betaPars, var=scevar)
-            
             
             return (list(scenario=scenario, seasonBroodEnv=list(parsBroodEnv=seasonBroods, broodEnv=seasonVar))) #, breedFail=breedFail))
           }
@@ -98,7 +87,6 @@ setMethod("run",
 )
 
 run.discretePopSim<- function(model){
-# print("Inside run.discretePop()")
   scenario<- S3Part(model)
   pars<- model@sim@params
 
@@ -122,16 +110,16 @@ run.discretePopSim<- function(model){
     ## Seasonality
     seasonVar<- seasonOptimCal(env=Env(scenario[i,]))[[1]] #, resolution=resolution, nSteps=seasonBroods$broods, interval=interval, criterion=criterion)
     if (any(seasonVar !=1)){
-      jMseason<- scenario$jM[i] * seasonVar
-      breedFailMseason<- scenario$breedFailM[i] * seasonVar
+      jindSeason<- scenario$jind[i] * seasonVar
+      jbrSeason<- scenario$jbr[i] * seasonVar
     }else{
-      jMseason<- scenario$jM[i]
-      breedFailMseason<- scenario$breedFailM[i]
+      jindSeason<- scenario$jind[i]
+      jbrSeason<- scenario$jbr[i]
     }
     
     for (n in 1:length(pars$N0)){
       N0<- pars$N0[n]
-      pop<- with(scenario[i,], discretePopSim.dispatch(broods=broods, b=b, j=jMseason, a=a, breedFail=breedFailMseason,
+      pop<- with(scenario[i,], discretePopSim.dispatch(broods=broods, b=b, j=jindSeason, a=a, breedFail=1 - jbrSeason,
                                               varJ=ifelse(pars$envVar$j, var, 0), varBreedFail=ifelse(pars$envVar$breedFail, var, 0),
                                               sexRatio=pars$sexRatio, matingSystem=pars$matingSystem, N0=N0, replicates=pars$replicates, tf=pars$tf))
       ## TODO: pop<- list(popF, popM)
@@ -167,7 +155,6 @@ run.discretePopSim<- function(model){
 }
 
 run.numericDistri<- function(model){
-  # print("Inside run.numericDistri()")
   scenario<- S3Part(model)
   pars<- model@sim@params
   
@@ -184,16 +171,19 @@ run.numericDistri<- function(model){
     ## Seasonality
     seasonVar<- seasonOptimCal(env=Env(scenario[i,]))[[1]] #, resolution=resolution, nSteps=seasonBroods$broods, interval=interval, criterion=criterion)
     if (any(seasonVar !=1)){
-      jMseason<- scenario$jM[i] * seasonVar
-      breedFailMseason<- scenario$breedFailM[i] * seasonVar
+      jindSeason<- scenario$jind[i] * seasonVar
+      jbrSeason<- scenario$jbr[i] * seasonVar
     }else{
-      jMseason<- scenario$jM[i]
-      breedFailMseason<- scenario$breedFailM[i]
+      jindSeason<- scenario$jind[i]
+      jbrSeason<- scenario$jbr[i]
     }
 
     for (n in 1:length(pars$N0)){
       N0<- pars$N0[n]
-      distri<- with(scenario[i,], t1distri_dispatch(broods=broods, b=b, j=jMseason, a=a, breedFail=breedFailMseason,
+      ## TODO: check error when breedFail == 1:
+      # only happens on run(model), not when calling mSurvBV.trans with the same parameters!!
+      # maybe var collide with var()?? It seems is not the case...
+      distri<- with(scenario[i,], t1distri_dispatch(broods=broods, b=b, j=jindSeason, a=a, breedFail=1 - jbrSeason,
                                               varJ=ifelse(pars$envVar$j, var, 0), varBreedFail=ifelse(pars$envVar$breedFail, var, 0),
                                               sexRatio=pars$sexRatio, matingSystem=pars$matingSystem, N=N0))
       
@@ -226,7 +216,7 @@ run.numericDistri<- function(model){
   return(simRes)
 }
 
-##
+
 setGeneric("result", function(model, type="stats") standardGeneric("result"))
 # Create a data frame with the aggregated results and parameters
 setMethod("result", 
@@ -235,14 +225,7 @@ setMethod("result",
             if (nrow(model@sim) == 0){
               stop("There are no results yet. Use run(model) to start the simulations. The function return a model with the results on the sim slot.\n")
             }
-#               if (all(is.na(as.numeric(res$scenario)))){ # for categoric scenarios in ssaLH-behavior
-#                 scenario<- strsplit(res$scenario, "_")
-#                 scenario<- do.call("rbind", scenario)
-#                 if (inherits(model@sim, "Sim.ssa")){
-#                   colnames(scenario)<- c("LH", "environment", "behavior")
-#                 }
-#                 res<- data.frame(scenario, res)
-#               }
+
             res<- switch(type,
               stats={
                 res<- merge(S3Part(model), S3Part(model@sim), by.x="row.names", by.y="scenario")
---
+++ test/juvenileMortality.r
@@ -0,0 +1,69 @@
+## How to decompose the mortality in two independent but sequential events 
+## accounting a fixed proportion of the total mortality?
+# breedFail is a proportion of juvenile mortality correlated at the brood level
+# http://en.wikipedia.org/wiki/Probability#Mathematical_treatment
+#           /\
+#          /  \	
+#         /    \
+#        /     jbr
+#       /      /\
+#      /      /  \
+#  1-jbr  1-jind jind
+#       \  /      |
+#        \/       |
+#       1-j       j
+
+# survival: P(j) = P(jbr ∩ jind) = P(jbr) * P(jind)
+# death:    P(-j) = P(-jbr ∪ P(-jind | jbr))
+# j= jbr * jind
+
+## d = 1 - j
+## dbr = d * breedFail
+## d = dbr + dind * (1-dbr) = dbr + dind - dind * dbr
+# dind = (dbr - d) / (dbr - 1)
+# dind = (d * breedFail - d) / (d * breedFail - 1)
+
+######################################################
+# dbr = d * breedFail                                #
+# dind = (d * breedFail - d) / (d * breedFail - 1)   #
+######################################################
+
+## 1 - jbr = (1 - j) * breedFail
+#  jbr = j * breedFail - breedFail + 1 = breedFail * (j-1) + 1
+## j = jbr - 1 - (1 - jind) * (1 - (1 - jbr)) + 1 = jbr - (1 - jind) * jbr = jbr - (jbr - jind*jbr) 
+# j = jbr * jind
+# jind = -1 - ((1 - jbr) - (1 - j)) / ((1 - jbr) - 1) = -1 - (j - jbr) / jbr = -1 - (j / jbr -1)
+# jind = j / jbr
+
+######################################
+# jbr = breedFail * (j-1) + 1        #
+# jind = j / (breedFail * (j-1) + 1) #
+######################################
+
+
+j<- seq(.1, .9, by=.1) # juvenile survival
+d<- 1 - j
+breedFail<- .7 # Proportion of j due to breeding fail
+
+## SCHEMA
+dbr = d * breedFail                                #
+dind = (d * breedFail - d) / (d * breedFail - 1)   #
+data.frame(j, jCalc=(1-dbr) * (1-dind), dCalc=dbr + dind * (1-dbr), jbr, jind)
+
+jbr = breedFail * (j-1) + 1
+jind = j / (breedFail * (j-1) + 1) 
+tmp<- data.frame(j, jCalc=jbr * jind, jbr, jind, dbr=1-jbr, dind=1-jind)
+
+## Compare deterministic and probability distribution models ----
+
+a=.6; j=.3; broods=2; b=4; fecundity=broods * b;
+N0<- 5
+
+i<- 1
+j<- tmp$j[i]
+jind<- tmp$jind[i]
+jbr<- tmp$jbr[i]
+sdistri(l<- lambda(mSurvBV.trans(broods=broods, b=b, j=jind, a=a, breedFail=1-jbr, N=N0), N0=N0))
+lambda(LefkovitchPre(a=a, bj=fecundity * j))
+
+
